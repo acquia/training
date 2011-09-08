c5f571f362f41f4cc1d4bf0af56e86be309f0f5e
Jesse Beach <splendidnoise@gmail.com>
2011-09-08 15:55:23 -0400

    Put some useful code into the basic.js files, rather than a hello world.
    Signed-off-by: Jesse Beach <splendidnoise@gmail.com>

--- themes/cornflower/js/basic.js
+++ themes/cornflower/js/basic.js
@@ -1,3 +1,25 @@
-jQuery(document).ready(function () {
-  console.log("hello");
-});
+/*jslint bitwise: true, eqeqeq: true, immed: true, newcap: true, nomen: false,
+ onevar: false, plusplus: false, regexp: true, undef: true, white: true, indent: 2
+ browser: true */
+
+/*global jQuery: true Drupal: true window: true */
+
+(function ($) {
+  /**
+   * custom object is created if it doesn't exist.
+   */
+  Drupal.behaviors.custom = Drupal.behaviors.custom || {};
+  
+  /**
+  * Attach handler.
+  */
+  Drupal.behaviors.custom = {
+    attach: function (context, settings) {
+      Drupal.behaviors.custom.exampleFunction(context, settings);
+    }
+  };
+  
+  /**
+   */
+  Drupal.behaviors.custom.exampleFunction = function (context, settings) {};
+})(jQuery);
