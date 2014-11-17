142d7f7852f6f73c3428a0a437b5592d1f8e118e
Heather <hjames@gmail.com>
2014-11-17 12:38:06 +0000

    Removed old files

--- themes/responsive/sparkle.js
+++
@@ -1,11 +0,0 @@
-(function ($) { 
-  Drupal.behaviors.responsiveAnimation = {
-    attach: function (context, settings) { 
-      $('#site-name', context)      .hover(function () {
-        $(this).animate({ 
-          'margin-left': '10px'
-        }, 'slow');
-      });
-    }
-  }
-})(jQuery);
